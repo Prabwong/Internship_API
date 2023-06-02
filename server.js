10582175157e8e7cefea8eb2e2b90e467ee2e72b
prabwong <prabwong@gmail.com>
2023-06-02 14:20:38 +0700

    API App

--- server.js
+++ server.js
@@ -1,8 +1,28 @@
-const con = require('./connection')
+const mysql = require('mysql2')
 const express = require('express')
 const app = express()
 
-//routes
+app.use(express.json())
+
+const con = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'Prab49973',
+    database: 'test'
+});
+
+con.connect((err) => {
+    if(err){
+        console.log("Connection Error");
+        console.log(err);
+    }else{
+        console.log("Connection to MySQL Database");
+    }
+});
+
+app.listen(3000, () => {
+    console.log('Node API app is running on port 3000')
+})
 
 app.get('/', (req, res) =>{
     res.send('Hello Node API')
@@ -18,6 +38,38 @@ app.get('/all-student', (req, res) =>{
     })
 })
 
-app.listen(3000, () => {
-    console.log('Node API app is running on port 3000')
+app.post('/add-student', (req, res) =>{
+    const data = req.body;
+    con.query("INSERT INTO Name_ID SET ?", data, (err, result) =>{
+        if(err){
+            res.send('Error');
+            console.log(err);
+        }else{
+            res.send(result);
+        }
+    })
+})
+
+app.put('/update-student/:id', (req, res) =>{
+    const data = [req.body.Full_name, req.body.Nickname, req.params.id];
+    con.query("UPDATE Name_ID SET Full_name = ?, Nickname = ? where ID = ?", data, (err, result) =>{
+        if(err){
+            res.send('Error');
+            console.log(err);
+        }else{
+            res.send(result);
+        }
+    })
+})
+
+app.delete('/delete-student/:id', (req, res) =>{
+    let student_id = req.params.id;
+    con.query("DELETE from Name_ID where ID = " + student_id, (err, result) =>{
+        if(err){
+            res.send('Error');
+            console.log(err);
+        }else{
+            res.send(result);
+        }
+    })
 })
