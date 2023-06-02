16c15d197e896cc355700b80c489a0e479cb908b
prabwong <prabwong@gmail.com>
2023-06-02 10:31:57 +0700

    API app

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const con = require('./connection')
 const express = require('express')
 const app = express()
 
@@ -7,6 +8,16 @@ app.get('/', (req, res) =>{
     res.send('Hello Node API')
 })
 
+app.get('/all-student', (req, res) =>{
+    con.query("select * from Name_ID", (err, result) =>{
+        if(err){
+            res.send('Error');
+        }else{
+            res.send(result);
+        }
+    })
+})
+
 app.listen(3000, () => {
-    console.log('Node API app is running on port 300')
+    console.log('Node API app is running on port 3000')
 })
